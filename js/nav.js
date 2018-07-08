5f242a90692ae10f3f60d389b142f0a3cf21171d
MateuszPeliński <pelinski.m@gmail.com>
2018-07-08 21:04:23 +0200

    dodany slider

--- js/nav.js
+++ js/nav.js
@@ -64,3 +64,11 @@ const makeActiveNavElement = id => {
     console.log(id)
     // code her ;)
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+        var simple = document.querySelector('.js_simple');
+
+        lory(simple, {
+            infinite: 1
+        });
+    });
