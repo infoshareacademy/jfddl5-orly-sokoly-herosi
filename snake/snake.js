cea2a3059982348a2286a7acb2b0afe0472e000d
Kacper Swietlaga <kacperres@gmail.com>
2018-07-19 17:03:16 +0200

    added-score-board

--- snake/snake.js
+++ snake/snake.js
@@ -15,7 +15,7 @@ function GameSnake() {
     this.initialPositionSnakeHead = this.initialPosition(6, 6)
     this.initialPositionSnakeBody1 = this.initialPosition(7, 6)
     this.initialPositionSnakeBody2 = this.initialPosition(8, 6)
-//	this.initialPositionSnakeBody3 = this.initialPosition(9, 6)  //dodanie ogona
+    //	this.initialPositionSnakeBody3 = this.initialPosition(9, 6)  //dodanie ogona
     this.initialPositionFood = this.initialPosition(3, 3)
     this.foodPosition = null
     this.score = null
@@ -45,8 +45,8 @@ GameSnake.prototype.prepareLayout = function () {
     this.container.appendChild(this.gameContainer)
 }
 //this function will be transform the table from js to HTML 
-GameSnake.prototype.render = function () { 
-	this.gameContainer.innerHTML = ''
+GameSnake.prototype.render = function () {
+    this.gameContainer.innerHTML = ''
 
     this.area.forEach(areaRow => {
         const row = this.makeRows()
@@ -59,16 +59,16 @@ GameSnake.prototype.render = function () {
         this.gameContainer.appendChild(row)
     })
 }
-  
+
 GameSnake.prototype.makeRows = () => {
     const row = document.createElement('div')
     row.classList.add('row')
-	row.style.display = 'flex'
+    row.style.display = 'flex'
     return row
 }
 
 GameSnake.prototype.makeCell = (element) => {
-	const GameSnakeElement = (creating) => () => {
+    const GameSnakeElement = (creating) => () => {
         const element = document.createElement('div')
         element.classList.add('cell-' + creating)
         return element
@@ -77,26 +77,71 @@ GameSnake.prototype.makeCell = (element) => {
     const CellHeadSnake = GameSnakeElement('snake-head')
     const CellBodySnake = GameSnakeElement('snake-body')
     const CellFood = GameSnakeElement('food')
-	const CellZero = GameSnakeElement('zero')
+    const CellZero = GameSnakeElement('zero')
 
     if (element === '0') {
         return CellZero()
     } else if (element === 'H') {
-        return	CellHeadSnake()
+        return CellHeadSnake()
     } else if (element === '1') {
         return CellBodySnake()
     } else if (element === 'F') {
         return CellFood()
     }
-	
+
 }
 
 GameSnake.prototype.addSnakeAndFoodToArea = function () {
     this.area[this.initialPositionSnakeHead.whatRow][this.initialPositionSnakeHead.whatColumn] = 'H'
     this.area[this.initialPositionSnakeBody1.whatRow][this.initialPositionSnakeBody1.whatColumn] = '1'
     this.area[this.initialPositionSnakeBody2.whatRow][this.initialPositionSnakeBody2.whatColumn] = '1'
-//	this.area[this.initialPositionSnakeBody3.whatRow][this.initialPositionSnakeBody3.whatColumn] = '1'  //ogon snejka
+    //	this.area[this.initialPositionSnakeBody3.whatRow][this.initialPositionSnakeBody3.whatColumn] = '1'  //ogon snejka
     this.area[this.initialPositionFood.whatRow][this.initialPositionFood.whatColumn] = 'F'
 }
 
-new GameSnake()
+const game1 = new GameSnake()
+
+GameSnake.prototype.addNickUserToRankingList = function (scoreObject, ulRef) {
+    const li = document.createElement('li')
+    ulRef.appendChild(li)
+    li.innerText = scoreObject.nick + ' : ' + scoreObject.score
+}
+
+
+GameSnake.prototype.addRankingElemnts = function () {
+    const rankingContainer = document.createElement('div')
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+    const ul = document.createElement('ul')
+
+    const scoreBoard = JSON.parse(localStorage.getItem('osh-snake-game')) || []
+    console.log(scoreBoard)
+
+    document.querySelector('body').appendChild(rankingContainer)
+    rankingContainer.appendChild(input)
+    rankingContainer.appendChild(button)
+    rankingContainer.appendChild(ul)
+
+    input.value = 'NICK'
+    button.innerText = 'confirm'
+    button.addEventListener(
+        'click',
+        () => {
+            const newScoreBoard = scoreBoard.concat({
+                    nick: input.value,
+                    score: this.score
+                })
+
+            localStorage.setItem('osh-snake-game', JSON.stringify(newScoreBoard))
+
+            input.remove()
+            button.remove()
+
+            newScoreBoard.forEach((el, i, arr) => {
+                this.addNickUserToRankingList(el,ul)
+             })
+            //this.addNickUserToRankingList(input, ul)
+        }
+    )
+}
+game1.addRankingElemnts()
